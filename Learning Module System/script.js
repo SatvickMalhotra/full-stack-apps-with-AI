6ddc0d5c07c2d22b376812a85c54cba4dca6f4d5
Satvick Malhotra <131777875+SatvickMalhotra@users.noreply.github.com>
2025-04-01 12:40:37 +0530

    Update script.js

--- Learning Module System/script.js
+++ Learning Module System/script.js
@@ -4,13 +4,13 @@ import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs
 
 // Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBfW3XyhYaM0smgflvCA-UI7i2vjxYEoaY",
-    authDomain: "lms-mswasth.firebaseapp.com",
-    databaseURL: "https://lms-mswasth-default-rtdb.firebaseio.com",
-    projectId: "lms-mswasth",
-    storageBucket: "lms-mswasth.firebasestorage.app",
-    messagingSenderId: "1060056773959",
-    appId: "1:1060056773959:web:b2c9117db3002d7cb5c862"
+    apiKey: "-",
+    authDomain: "-..com",
+    databaseURL: "https://---rtdb..com",
+    projectId: "-",
+    storageBucket: "..app",
+    messagingSenderId: "",
+    appId: "1::web:"
 };
 
 // Initialize Firebase
